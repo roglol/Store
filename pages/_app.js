5717242600f0ee09c8c5ca74a955ec7562c69d63
raphaelQuivel <otikopotskhoraia@gmail.com>
2020-01-28 18:55:17 +0400

    first

---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import App from 'next/app';
+import Head from 'next/head'
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class MyApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+        return {
+            pageProps: {
+                ...(Component.getInitialProps
+                    ? await Component.getInitialProps(ctx)
+                    : {})
+            }
+        };
+    }
+
+    render() {
+        const { Component, pageProps, store } = this.props;
+        return (
+            <>
+                    <Head>
+                        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossOrigin="anonymous"/>
+                    </Head>
+                    <Component {...pageProps} />
+             </>
+        );
+    }
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from '../styles/index.css';
+
+const Index = () =>{
+    return (
+        <div className={css.example}>
+            vaimee
+        </div>
+    )
+}
+
+export default Index;
