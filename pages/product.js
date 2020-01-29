eba536fcafab596113857b21a522a1516cfd2416
roglol <otikopotskhoraia@gmail.com>
2020-01-30 00:21:37 +0400

    DB

---
+++ pages/product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import product from '../styles/product.css';
+
+const Product = () =>{
+    return (
+        <div className={product.example}>
+            vaimee
+        </div>
+    )
+}
+
+export default Product;
--- server.js
+++ server.js
@@ -4,16 +4,39 @@ const express = require('express')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'store'
+  });
 
+  con.connect(function(err) {
+    if (err) throw err
+    console.log('You are now connected with mysql database...')
+  })
 
+ 
 
 app.prepare()
     .then(() => {
         const server = express()
+        var router = express.Router();
+        server.use('/api',  router)
         server.use('/public', express.static(__dirname + '/public'));
 
         // server.get('/favicon.ico', (req, res) => res.status(204));
+
+
+        router.get('/products', (req,res) =>{
+                con.query("SELECT * FROM products WHERE NAME='gialo' OR  NAME='fridge'", function (err, result, fields) {
+                  if (err) throw err;
+                  res.send(result)
+                });
+        })
+
         server.get('*', (req,res) =>{
             return handle(req,res)
         })
